3e8030093dc4718374d77cc76bcc0867076e245b
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-02 22:42:11 -0300

    feat: update create and exception

--- order-service/src/app/app.module.ts
+++ order-service/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
@@ -17,12 +17,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatMenuModule } from '@angular/material/menu';
+
 import { HeaderComponent } from './views/components/template/header/header.component';
 import { FooterComponent } from './views/components/template/footer/footer.component';
 import { NavComponent } from './views/components/template/nav/nav.component';
 import { HomeComponent } from './views/components/home/home.component';
 import { TecnicoReadComponent } from './views/components/tecnico/tecnico-read/tecnico-read.component';
-import { MatPaginatorModule } from '@angular/material/paginator';
 import { TecnicoCreateComponent } from './views/components/tecnico/tecnico-create/tecnico-create.component';
 
 
@@ -55,7 +58,10 @@ import { TecnicoCreateComponent } from './views/components/tecnico/tecnico-creat
     MatInputModule,
     MatDatepickerModule,
     FormsModule,
+    ReactiveFormsModule,
     MatPaginatorModule,
+    MatSnackBarModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- order-service/src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ order-service/src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,5 +1,7 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TecnicoService } from 'src/app/services/tecnico.service';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -8,13 +10,35 @@ import { Router } from '@angular/router';
 })
 export class TecnicoCreateComponent implements OnInit {
 
-  constructor(private router : Router ) { }
+  tecnico: Tecnico = {
+    id: '',
+    nome:'Davi',
+    cpf:'678.520.570-04',
+    telefone: '(00) 91111-0000'
+  }
+
+  constructor(
+    private router : Router,
+    private service: TecnicoService
+     ) { }
 
   ngOnInit(): void {
   }
 
   cancel():void{
     this.router.navigate(['tecnicos'])
+  }
+
+  create():void{
+    this.service.create(this.tecnico).subscribe((resposta) => {
+      this.router.navigate(['tecnicos'])
+      this.service.message('Tecnico criado com sucesso!')
+    }, err => {
+      console.log(err)
+      if(err.error.error.match('já cadastrado')) {
+        this.service.message(err.error.error)
+      }
+    })
   }
 
 }
