84b624715e172afc96204eef8a5d310e6605601b
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-05 22:36:07 -0300

    feat: component os read

--- order-service/src/app/views/components/os/os-read/os-read.component.ts
+++ order-service/src/app/views/components/os/os-read/os-read.component.ts
@@ -3,7 +3,9 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { OS } from 'src/app/models/os';
+import { ClienteService } from 'src/app/services/cliente.service';
 import { OsService } from 'src/app/services/os.service';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-os-read',
@@ -21,7 +23,9 @@ export class OsReadComponent implements AfterViewInit {
 
   constructor(
     private service : OsService,
-    private router : Router){}
+    private router : Router,
+    private tecnicoService: TecnicoService,
+    private clienteService: ClienteService) {}
 
   ngAfterViewInit() {
     this.findAll();
@@ -30,6 +34,8 @@ export class OsReadComponent implements AfterViewInit {
   findAll():void {
     this.service.findAll().subscribe((resposta) => {
       this.lista = resposta;
+      this.listTecnico();
+      this.listCliente();
       this.dataSource = new MatTableDataSource<OS>(this.lista);
       this.dataSource.paginator = this.paginator;
     })
@@ -37,6 +43,22 @@ export class OsReadComponent implements AfterViewInit {
 
   navigateToCreate():void {
     this.router.navigate(['os/create'])
+  }
+
+  listTecnico():void {
+    this.lista.forEach( x => {
+      this.tecnicoService.findById(x.tecnico).subscribe(resposta => {
+        x.tecnico = resposta.nome
+      })
+    })
+  }
+
+  listCliente():void {
+    this.lista.forEach( x => {
+      this.clienteService.findById(x.cliente).subscribe(resposta => {
+        x.cliente = resposta.nome
+      })
+    })
   }
 
 }
